7647526fb6a2262006e921060292739ad6c642be
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-07-01 10:34:53 +0530

    taking user input

--- ArrayPractice3/Program.cs
+++ ArrayPractice3/Program.cs
@@ -2,13 +2,22 @@
 
 namespace ArrayPractice3
 {
+    //Take input from usere
     // Display Multidimentional array
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[,] arr1 = { { 2,3} ,{ 4,5} };
+            int[,] arr1 = new int[2, 2];
+            Console.WriteLine("Specify members of Array :");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
             
             for (int i = 0; i < 2; i++)
             {
