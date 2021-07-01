8b2a497758b03cf293646634c4c379e704e77500
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-07-01 09:59:30 +0530

    Add project files.

---
+++ ArrayPractice3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayPractice3
+{
+    // Display Multidimentional array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[,] arr1 = { { 2,3} ,{ 4,5} };
+            
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr1[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
